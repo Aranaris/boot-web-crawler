051fdf39066451f2fc8ecea32a704fb74368e99a
vinceye <vinceye.cm@gmail.com>
2024-01-31 14:00:33 -0800

    added cmd line arg input

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const { argv } = require('node:process')
+
+function main() {
+	if (argv.length !== 3) {
+		console.log('expected 1 argument!')
+	} else {
+		console.log(`base URL is: ${argv[2]}`)
+	}
+}
+
+main()
